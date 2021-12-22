6321facd7cadd9cec3b68f1aeb12a7c6cd0feec9
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-12-22 20:39:49 -0300

    first commit

---
+++ JDBCTest/src/JDBCTest.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class JDBCTest {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			System.out.println("Connecting to the database...");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "Senha123");
+			System.out.println("Conncetion established.");
+			
+			Statement st = conn.createStatement();
+			ResultSet rs1 = st.executeQuery("select * from C##DEV.tb_teste");
+			System.out.println("\n >>TB_TESTE:");
+			while(rs1.next()) {
+				System.out.println(rs1.getInt("TEST_ID")+" --> "+rs1.getString("TEST_NAME"));
+			}
+			ResultSet rs2 = st.executeQuery("select * from C##DEV.tb_user");
+			System.out.println("\n >>TB_USER:");
+			while(rs2.next()) {
+				System.out.println(rs2.getInt("USER_ID")+" "+rs2.getString("USER_FIRSTNAME")+" "+rs2.getString("USER_LASTNAME") +" "+rs2.getString("USER_EMAIL") +" "+rs2.getString("USER_PHONE"));
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+}
