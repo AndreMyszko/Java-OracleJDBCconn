a5a7773e1c0f4fbd72e3934ea6ab8343e1f7ea07
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-12-22 21:00:08 -0300

    add. close conn

--- JDBCTest/src/JDBCTest.java
+++ JDBCTest/src/JDBCTest.java
@@ -13,15 +13,18 @@ public class JDBCTest {
 			
 			Statement st = conn.createStatement();
 			ResultSet rs1 = st.executeQuery("select * from C##DEV.tb_teste");
-			System.out.println("\n >>TB_TESTE:");
+			System.out.println("\n>>TB_TESTE:");
 			while(rs1.next()) {
 				System.out.println(rs1.getInt("TEST_ID")+" --> "+rs1.getString("TEST_NAME"));
 			}
 			ResultSet rs2 = st.executeQuery("select * from C##DEV.tb_user");
-			System.out.println("\n >>TB_USER:");
+			System.out.println("\n>>TB_USER:");
 			while(rs2.next()) {
 				System.out.println(rs2.getInt("USER_ID")+" "+rs2.getString("USER_FIRSTNAME")+" "+rs2.getString("USER_LASTNAME") +" "+rs2.getString("USER_EMAIL") +" "+rs2.getString("USER_PHONE"));
 			}
+			
+			conn.close();
+			System.out.println("\nConnection closed.");
 			
 			
 		} catch (ClassNotFoundException e) {
